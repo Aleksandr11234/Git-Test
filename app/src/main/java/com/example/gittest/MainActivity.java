54f987f017195710024ca9c2664fc5bd6448f521
aleksandr_k <web17@themarat.com>
2021-06-04 09:50:42 +0300

    First Update

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onTestButtonClick(View view) {
+        Toast.makeText(this, "Test Button Click", Toast.LENGTH_LONG).show();
+    }
+
 }
